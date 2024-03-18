f96263a63826c3ec6a7f3d2fc268f55882d5ba69
luotuoqingshan <2019hyf@gmail.com>
2024-03-18 14:25:46 -0400

    use builtin triu for sym -> triu

--- src/preprocess.jl
+++ src/preprocess.jl
@@ -1,28 +1,4 @@
 """
-    sparse_sym_to_triu(I, J, V)
-
-Extract the upper triangular part of a symmetric sparse matrix in the (I, J, V) format.
-Both of the input and output are in the (I, J, V) format.
-"""
-function sparse_sym_to_triu(
-    I::Vector{Ti}, 
-    J::Vector{Ti},
-    V::Vector{Tv},
-)where{Ti <: Integer, Tv <: AbstractFloat}
-    triu_I, triu_J, triu_V = Ti[], Ti[], Tv[]
-    for i in eachindex(I)
-        # only store upper triangular part
-        if I[i] <= J[i] 
-            push!(triu_I, I[i])
-            push!(triu_J, J[i])
-            push!(triu_V, V[i])
-        end
-    end
-    return triu_I, triu_J, triu_V
-end
-
-
-"""
     preprocess_sparsecons(As)
 
 Preprocess the sparse constraints to initialize necessary 
@@ -33,43 +9,44 @@ function preprocess_sparsecons(
 ) where {Tv <: AbstractFloat, Ti <: Integer}
     # aggregate all constraints into one sparse matrix
 
+    n = size(As[1], 1)
+    nA = length(As)
+
+    total_nnz = 0; total_triu_nnz = 0
+    for A in As
+        total_nnz += nnz(A)
+        total_triu_nnz += nnz(triu(A))
+    end
+    
+
     # (all_triu_I, all_triu_J, all_triu_V) stores 
     # combined upper triangular part of all constraint matrices A
-    all_triu_I, all_triu_J, all_triu_V = Ti[], Ti[], Tv[]
+    all_triu_I = zeros(Ti, total_triu_nnz)
+    all_triu_J = zeros(Ti, total_triu_nnz)
+    all_triu_V = ones(Tv, total_triu_nnz)
 
     # (all_I, all_J, all_V) stores
     # combined entries of all constraint matrices A
-    all_I, all_J, all_V = Ti[], Ti[], Tv[]
+    all_I = zeros(Ti, total_nnz)
+    all_J = zeros(Ti, total_nnz)
+    all_V = ones(Tv, total_nnz)
 
-    n = size(As[1], 1)
-    nA = length(As)
-    total_nnz = 0
-
-    # (triu_I_list, triu_J_list, triu_V_list) stores
-    # upper triangular part of each constraint matrix A
-    triu_I_list = Vector{Ti}[]
-    triu_J_list = Vector{Ti}[]
-    triu_V_list = Vector{Tv}[]
+    cur_ptr = 0
+    cur_triu_ptr = 0
 
     for A in As
         # extract upper triangular part of A
-        I, J, V = findnz(A)
-        triu_I, triu_J, triu_V = 
-            sparse_sym_to_triu(I, J, V)
-        push!(triu_I_list, triu_I)
-        push!(triu_J_list, triu_J)
-        push!(triu_V_list, triu_V)
+        I, J, _ = findnz(A)
+        triu_I, triu_J, _ = findnz(triu(A))
 
         # here we append 1 instead of V[i] to both all_triu_V and all_V
         # in this way we can make sure cancellation doesn't happen 
-        append!(all_triu_I, triu_I)
-        append!(all_triu_J, triu_J)
-        append!(all_triu_V, ones(Tv, length(triu_V)))
-
-        append!(all_I, I)
-        append!(all_J, J)
-        append!(all_V, ones(Tv, length(V)))
-        total_nnz += length(triu_V)
+        all_I[cur_ptr+1:cur_ptr+length(I)] .= I
+        all_J[cur_ptr+1:cur_ptr+length(J)] .= J
+        all_triu_I[cur_triu_ptr+1:cur_triu_ptr+length(triu_I)] .= triu_I
+        all_triu_J[cur_triu_ptr+1:cur_triu_ptr+length(triu_J)] .= triu_J
+        cur_ptr += length(I)
+        cur_triu_ptr += length(triu_I)
     end
 
     # nnz of agg_sparse_A correspond to potential nnz of 
@@ -84,18 +61,16 @@ function preprocess_sparsecons(
     agg_sparse_A = sparse(all_I, all_J, all_V, n, n)
 
     agg_sparse_A_matptr = zeros(Ti, nA + 1)
-    agg_sparse_A_nzind = zeros(Ti, total_nnz)
-    agg_sparse_A_nzval_one = zeros(Tv, total_nnz)
-    agg_sparse_A_nzval_two = zeros(Tv, total_nnz)
+    agg_sparse_A_nzind = zeros(Ti, total_triu_nnz)
+    agg_sparse_A_nzval_one = zeros(Tv, total_triu_nnz)
+    agg_sparse_A_nzval_two = zeros(Tv, total_triu_nnz)
 
     cumul_nnz = 0
     for i in eachindex(As)
         # entries from agg_A_ptr[i] to agg_A_ptr[i+1]-1
         # correspond to the i-th sparse constraint/objective matrix
         agg_sparse_A_matptr[i] = cumul_nnz + 1
-        triu_I = triu_I_list[i]
-        triu_J = triu_J_list[i]
-        triu_V = triu_V_list[i]
+        triu_I, triu_J, triu_V = findnz(triu(As[i]))
         for j in eachindex(triu_I)
             row, col = triu_I[j], triu_J[j]
             low = triu_agg_sparse_A.colptr[col]
@@ -122,7 +97,7 @@ function preprocess_sparsecons(
         end
         cumul_nnz += length(triu_I)
     end
-    agg_sparse_A_matptr[end] = total_nnz + 1
+    agg_sparse_A_matptr[end] = total_triu_nnz + 1
     agg_sparse_A_mappedto_triu = zeros(Ti, length(agg_sparse_A.rowval))
     for col = 1:n
         for nzi = agg_sparse_A.colptr[col]:agg_sparse_A.colptr[col+1]-1
