ec52a31ba9f863c509f61cf1e68d010a5203291c
luotuoqingshan <2019hyf@gmail.com>
2024-03-18 16:40:39 -0400

    add different variants of the graph data (let all edge weights be positive, add dummy nodes to the graph for MinimumBisection) add automated preprocessing for gset

--- src/data_preprocess.jl
+++ src/data_preprocess.jl
@@ -88,6 +88,37 @@ function read_zip(
 end
 
 
+function read_gset(
+    filename::String;
+    filefolder::String=homedir()*"/Gset/",
+)::SparseMatrixCSC
+    I = Int32[] 
+    J = Int32[]
+    V = Float64[]
+    n = 0
+    filepath = filefolder*filename
+    open(filepath) do file
+        lines = readlines(file)
+        n, _ = split(lines[1], ' ')
+        n = parse(Int, n)
+        for line in lines[2:end]
+            u, v, w = split(line, ' ')
+            u = parse(Int, u)
+            v = parse(Int, v)
+            w = parse(Float64, w)
+            # Turn it into an undirected graph
+            push!(I, u); push!(J, v); push!(V, w)
+            push!(J, u); push!(I, v); push!(V, w)
+        end
+    end
+    A = sparse(I, J, V, n, n)
+    # remove self-loops
+    A[diagind(A)] .= 0
+    dropzeros!(A)
+    return A
+end
+
+
 function postprocess_graph(A)
     B = deepcopy(A)
 
@@ -135,4 +166,30 @@ for dataset in ["web-BerkStan", "web-Google", "web-NotreDame", "web-Stanford"]
     A = max.(A, A')
     B = postprocess_graph(A)
     matwrite(homedir()*"/datasets/graphs/"*dataset*".mat", Dict("A" => B))
+end
+
+for i in [(1:67)..., 70, 72, 77, 81]
+    dataset = "G$i" 
+    @info dataset
+    A = read_gset(dataset)
+    matwrite(homedir()*"/datasets/graphs/"*dataset*".mat", Dict("A" => A))
+end
+
+for (root, dirs, files) in walkdir(homedir()*"/datasets/graphs/")
+    for file in files
+        @info file
+        data = matread(root*"/"*file)
+        A = data["A"]
+        A_abs = abs.(A)
+        A_dummy = A
+        n = size(A, 1)
+        if mod(n, 2) == 1
+            I, J, V = findnz(A)
+            A_dummy = sparse(I, J, V, n+1, n+1)
+        end
+        A_dummy_abs = abs.(A_dummy)
+        matwrite(root*"/"*file,
+            Dict("A" => A, "A_abs" => A_abs, "A_dummy" => A_dummy, "A_dummy_abs" => A_dummy_abs)
+        )
+    end
 end
