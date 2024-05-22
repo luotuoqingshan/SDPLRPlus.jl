aad5709a2760db45097c3c63d6002d02698ebe5e
luotuoqingshan <2019hyf@gmail.com>
2024-05-22 10:39:37 -0400

    update

--- exp_csdp/exp_csdp.jl
+++ exp_csdp/exp_csdp.jl
@@ -1,7 +1,8 @@
 using CSDP
-using JSON
+using JSON, MAT
 using Random
-using Arpack
+using LinearAlgebra, Arpack 
+using SDPLR: SymLowRankMatrix
 
 function csdp_solver(
     C::TC,
@@ -55,9 +56,6 @@ function csdp_solver(
         "extra_fval" => extra_fval,
     )
     mkpath(filefolder)
-    #open(filefolder*filename*"-seed-$seed.json", "w") do f
-    #    JSON.print(f, res, 4)
-    #end
     open(filefolder*filename*"-short-seed-$seed-tol-$(params[:axtol]).json", "w") do f
         JSON.print(f, short_res, 4)
     end
@@ -85,8 +83,6 @@ function default_params()
     )
 end
 
-
-include("header.jl")
 using ArgParse
 
 s = ArgParseSettings()
@@ -112,35 +108,79 @@ s = ArgParseSettings()
         arg_type = Float64
         default = 1e-2
         help = "Tolerance for relative duality gap"
+    "--seed"
+        arg_type = Int
+        default = 0
+        help = "Random seed"
 end
 
-function maxcut_rounding(C, X)
-    Z = svds(X, nsv=10)[1]
+function read_graph(name, problem)
+    if problem in ["LovaszTheta"]
+        problem = "MaxCut"
+    end
+    return matread(homedir()*"/datasets/graphs/"*problem*"/"*name*".mat")["A"]
+end
+
+function eval_cut(L, x)
+    # make sure x is a vector of +1 and -1
+    return 0.25 * x' * L * x
+end
+
+function maxcut_rounding(C, Rt)
+    R = Rt'
+    r = size(R, 2)
     # take 100 cuts
-    best_cut = 0
-    for i = 1:100
-        z = sign.(Z.U * randn(10))
-        best_cut = max(best_cut, z' * C * z)
+    best_cut = -Inf
+    L = 4 * C
+    for _ = 1:100
+        z = sign.(R * randn(r))
+        best_cut = max(best_cut, eval_cut(L, z))
     end
     return best_cut
 end
 
+function minimumbisection_rounding(C, Rt)
+    R = Rt'
+    r = size(R, 2)
+    # take 100 cuts
+    L = -4 * C
+    best_cut = Inf
+    for _ = 1:100
+        z = R * randn(r)
+        perm = sortperm(z)
+        n = length(perm)
+        part = zeros(n)
+        part[perm] .= [i*2 <= n for i=1:n]*2 .- 1
+        @assert sum(part) == 0
+        best_cut = min(best_cut, eval_cut(L, part))
+    end
+    return best_cut
+end
+
+identity(C, Rt) = Inf
+
 args = parse_args(s)
+Random.seed!(args["seed"])
 BLAS.set_num_threads(1)
 
+include("problems.jl")
+
 if args["problem"] == "MaxCut"
-    A = read_graph(args["graph"])["A_abs"]
+    A = read_graph(args["graph"], args["problem"])
     C, As, bs = maxcut(A)
     extra_f = maxcut_rounding
 elseif args["problem"] == "MinimumBisection"
-    A = read_graph(args["graph"])["A_dummy_abs"]
+    A = read_graph(args["graph"], args["problem"])
     C, As, bs = minimum_bisection(A)
+    extra_f = minimumbisection_rounding
 elseif args["problem"] == "LovaszTheta"
-    A = read_graph(args["graph"])["A_abs"]
+    A = read_graph(args["graph"], args["problem"])
     C, As, bs = lovasz_theta(A)
-elseif args["problem"] == "Cutnorm"
-    A = read_graph(args["graph"])["A"]
+    extra_f = identity
+elseif args["problem"] == "CutNorm"
+    A = read_graph(args["graph"], args["problem"])
     C, As, bs = cutnorm(A)
+    extra_f = identity
 else
     error("Problem not supported")
 end
