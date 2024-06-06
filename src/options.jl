87bbe9ba438dd2fa3dc411077e59200a475d46b6
luotuoqingshan <2019hyf@gmail.com>
2024-06-05 20:04:03 -0400

    update options

--- src/options.jl
+++ src/options.jl
@@ -1,16 +1,16 @@
 @with_kw mutable struct BurerMonteiroConfig{Ti <: Integer, Tv}
-    ptol::Tv = 1e-2
-    objtol::Tv = 1e-2
-    σfac::Tv = 2.0 
-    maxtime::Tv = 3600.0
-    printlevel::Ti = 1
-    printfreq::Tv = 60.0
-    numlbfgsvecs::Ti = 4 
-    maxmajoriter::Ti = 10^5
-    maxiter::Ti = 10^7
-    fprec::Tv = 1e8 # for moderate accuracy
-    rankupd_tol::Ti = 4
-    prior_trace_bound::Tv = 1e18
-    dataset::String = ""
-    eval_DIMACS_errs::Bool = false
+    ptol::Tv = 1e-2                    # primal infeasibility tolerance 
+    objtol::Tv = 1e-2                  # suboptimality tolerance
+    σfac::Tv = 2.0                     # factor for increasing σ
+    maxtime::Tv = 3600.0               # maximum time in seconds(better use terminal tool for limiting time)
+    printlevel::Ti = 1                 # print level
+    printfreq::Tv = 60.0               # how often to print in seconds
+    numlbfgsvecs::Ti = 4               # number of L-BFGS vectors
+    maxmajoriter::Ti = 10^5            # maximum number of major iterations
+    maxiter::Ti = 10^7                 # maximum number of total iterations
+    fprec::Tv = 1e8                    # for moderate accuracy
+    rankupd_tol::Ti = 4                # rank update tolerance
+    prior_trace_bound::Tv = 1e18       # a trace bound determined or estimated
+    dataset::String = ""               # dataset name
+    eval_DIMACS_errs::Bool = false     # whether to evaluate DIMACS errors
 end
