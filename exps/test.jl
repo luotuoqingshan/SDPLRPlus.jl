832cbc8b5384f7eeecca77cb64787839f48e15be
luotuoqingshan <2019hyf@gmail.com>
2024-06-10 16:21:36 -0400

    update test.jl

--- exps/test.jl
+++ exps/test.jl
@@ -27,6 +27,14 @@ s = ArgParseSettings()
         arg_type = Int
         default = 10
         help = "Initial rank"
+    "--problem"
+        arg_type = String
+        default = "MaxCut"
+        help = "Problem type"
+    "--nthreads"
+        arg_type = Int
+        default = 1
+        help = "Number of threads. For benchmarking, we recommend 1."
 end
 
 args = parse_args(s)
@@ -35,9 +43,27 @@ seed = args["seed"]
 tol = args["ptol"]
 
 Random.seed!(seed)
-BLAS.set_num_threads(1)
+BLAS.set_num_threads(args["nthreads"])
 
+"""
+check env to ensure the number of threads is set to 1
+when benchmarking.
+"""
+function nthread_check()
+    if VERSION >= v"1.7"
+        @show LinearAlgebra.BLAS.get_config()
+    end
+    @show LinearAlgebra.BLAS.get_num_threads()
+    @show get(()->"", ENV, "OPENBLAS_NUM_THREADS")
+    @show get(()->"", ENV, "GOTO_NUM_THREADS")
+    @show get(()->"", ENV, "OMP_NUM_THREADS")
+    @show get(()->"", ENV, "MKL_NUM_THREADS")
+    @show get(()->"", ENV, "OPENBLAS_BLOCK_FACTOR")
+    @show Threads.nthreads()
+end
 
+# comment out if you don't want this info
+nthread_check()
 
 logger = SimpleLogger(stdout, Logging.Info)
 old_logger = global_logger(logger)
@@ -47,7 +73,8 @@ function eval_cut(L, x)
     return 0.25 * x' * L * x
 end
 
-function maxcut_rounding(L, Rt)
+function maxcut_rounding(A, Rt)
+    L = Diagonal(sum(A, dims=1)[:]) - A
     R = Rt'
     r = size(R, 2)
     # take 100 cuts
@@ -59,42 +86,79 @@ function maxcut_rounding(L, Rt)
     return best_cut
 end
 
-function batch_eval_maxcut(name, filename::String, r=10; kwargs...)
-    # warmup
-    @info "Running max cut on $name."
-    A = matread(homedir()*"/datasets/graphs/MaxCut/"*name*".mat")["A"] 
-    C, As, bs = maxcut(A)
-    n = size(A, 1)
-    res = sdplr(C, As, bs, r; prior_trace_bound=Float64(n), dataset=name, kwargs...)
+
+function minimumbisection_rounding(A, Rt)
     L = Diagonal(sum(A, dims=1)[:]) - A
-    best_cut = maxcut_rounding(L, res["Rt"])
-    res["best_cut"] = best_cut
-    @show name, res["totaltime"], res["primaltime"], res["rel_duality_bound"]
+    R = Rt'
+    r = size(R, 2)
+    # take 100 cuts
+    best_cut = Inf
+    for _ = 1:100
+        z = R * randn(r)
+        perm = sortperm(z)
+        n = length(perm)
+        part = zeros(n)
+        part[perm] .= [i*2 <= n for i=1:n]*2 .- 1
+        @assert sum(part) == 0
+        best_cut = min(best_cut, eval_cut(L, part))
+    end
+    return best_cut
+end
+
+dymmy_callback(A, Rt) = 0
+
+function batch_eval(
+    problem, 
+    graph, 
+    A, 
+    input::Function,
+    callback::Function,
+    filename::String, 
+    trace_bound,
+    r=10; 
+    kwargs...)
+    # warmup
+    @info "Running max cut on $graph."
+    C, As, bs = input(A)
+    res = sdplr(C, As, bs, r; prior_trace_bound=Float64(trace_bound), dataset=graph, kwargs...)
+    callback_res = callback(A, res["Rt"])
+    res["callback_res"] = callback_res
+    @show graph, res["totaltime"], res["primaltime"], res["rel_duality_bound"]
 
     short_res_keys = ["grad_norm", "primal_vio", "obj", "rel_duality_bound", 
         "totaltime", "dual_lanczos_time", "dual_GenericArpack_time", 
         "primaltime", "iter", "majoriter", "ptol", "objtol", "fprec", 
-        "best_cut", "rankupd_tol", "r",]
+        "callback_res", "rankupd_tol", "r",]
     short_res = Dict(k => res[k] for k in short_res_keys)
 
-    output_folder = homedir()*"/SDPLR.jl/output/MaxCut/"
-    mkpath(output_folder*"$name/SDPLR/")
-    open(output_folder*"$name/SDPLR/"*filename*".json", "w") do f
-        res["git commit"] = strip(read(`git rev-parse --short HEAD`, String))
+    output_folder = homedir()*"/SDPLRPlus.jl/exps/output/$problem/"
+    mkpath(output_folder*"$graph/")
+    open(output_folder*"$graph/"*filename*".json", "w") do f
+        #short_res["git commit"] = strip(read(`git rev-parse --short HEAD`, String))
         JSON.print(f, short_res, 4)
     end
 end
 
-if VERSION >= v"1.7"
-    @show LinearAlgebra.BLAS.get_config()
-  end
-  @show LinearAlgebra.BLAS.get_num_threads()
-  @show get(()->"", ENV, "OPENBLAS_NUM_THREADS")
-  @show get(()->"", ENV, "GOTO_NUM_THREADS")
-  @show get(()->"", ENV, "OMP_NUM_THREADS")
-  @show get(()->"", ENV, "MKL_NUM_THREADS")
-  @show get(()->"", ENV, "OPENBLAS_BLOCK_FACTOR")
-  @show Threads.nthreads()
+problem = args["problem"]
+graph = args["graph"]
+ind = findfirst(x -> x == problem, ["MaxCut", "MinimumBisection", "LovaszTheta", "CutNorm"])
+inputs = [maxcut, minimum_bisection, lovasz_theta, cutnorm]
+callbacks = [maxcut_rounding, minimumbisection_rounding, dymmy_callback, dymmy_callback]
+
+A = matread(homedir()*"/SDPLRPlus.jl/exps/data/$problem/G1.mat")["A"] 
+n = size(A, 1)
+trace_bounds = [n, n, 1, n] 
+
 # warmup
-batch_eval_maxcut("G1", "SDPLR-warmup", args["rank"]; maxtime=36000.0, objtol=args["objtol"], ptol=args["ptol"])
-VSCodeServer.@profview batch_eval_maxcut("$(args["graph"])", "SDPLR-R-$(args["rank"])-seed-$seed-tol-$tol", args["rank"]; maxtime=36000.0, objtol=args["objtol"], ptol=args["ptol"])
+# this is necessay for benchmarking julia code
+batch_eval(problem, "G1", A, inputs[ind], callbacks[ind], "SDPLR-warmup", trace_bounds[ind], args["rank"]; 
+                  maxtime=36000.0, objtol=args["objtol"], ptol=args["ptol"])
+
+
+A = matread(homedir()*"/SDPLRPlus.jl/exps/data/$problem/$graph.mat")["A"] 
+n = size(A, 1)
+trace_bounds = [n, n, 1, n] 
+# run the benchmark
+batch_eval(problem, graph, A, inputs[ind], callbacks[ind],  
+         "SDPLR-R-$(args["rank"])-seed-$seed-tol-$tol", trace_bounds[ind], args["rank"]; 
+         maxtime=36000.0, objtol=args["objtol"], ptol=args["ptol"])
