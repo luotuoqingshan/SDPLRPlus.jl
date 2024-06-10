65b380484a7615612b45c983c495c4448a1c8311
luotuoqingshan <2019hyf@gmail.com>
2024-06-10 17:03:14 -0400

    update generator for batch test

--- exps/gen_test_maxcut.jl
+++ exps/gen_test_maxcut.jl
@@ -1,11 +1,15 @@
 graphs = ["G$i" for i = 1:9] 
 seed = 0
+problem = "MaxCut" # support ["MaxCut", "MinimumBisection", "LovaszTheta", "CutNorm]
+tol = 0.01
+initial_rank = 10
 
-open(homedir()*"/SDPLR.jl/exps/test_maxcut.txt", "w") do io
+open(homedir()*"/SDPLRPlus.jl/exps/batch_test.txt", "w") do io
     for graph in graphs
         println(io, "ulimit -d $((16 * 1024 * 1024)); "*
-        "cd ~/SDPLR.jl/exps; "*
+        "cd ~/SDPLRPlus.jl/exps; "*
         "/p/mnt/software/julia-1.10/bin/julia "*
-        "--project test_maxcut.jl --seed $seed --graph \"$graph\"")
+        "--project test.jl --seed $seed --graph \"$graph\" --problem \"$problem\" "*
+        "--ptol $tol --objtol $tol --rank $initial_rank")
     end
 end
